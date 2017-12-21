0f0899e9e1269b3718313875305f2f9edeed605a
Adil <adilserkerov@gmail.com>
2017-12-21 17:33:56 +0300

    pfwf

---
+++ public/javascripts/script.js
@@ -0,0 +1,75 @@
+window.onload = function (){
+    
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    
+    let ball = {
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 0,
+        positionX: -1000,
+        positionY: 0,
+        positionZ: -500
+    }
+
+    let canvas = document.getElementById("canvas");
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+    let renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+    renderer.setClearColor(0x000000);
+
+    let scene = new THREE.Scene();
+
+    let light = THREE.AmbientLight(0xffffff);
+    scene.add(light);
+
+
+    let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+    camera.position.set(0, 0, 1000);
+
+    let geometry = new THREE.SphereGeometry(50, 15, 15);
+    let material = new THREE.MeshBasicMaterial({color: 0xffffff, vertexColors: THREE.FaceColors});
+
+    function r(){
+        let rand = Math.random();
+        if(rand > 0.8 && rand < 0.2)
+            return r();
+        return rand;
+    }
+
+    let t0 = 0,
+            deltaTime;
+    function time() {
+        let now = performance.now();
+        deltaTime = now-t0;
+        t0 = now;
+        return deltaTime;
+    }
+
+
+    let mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    function objectMove(obj){
+        obj.positionX += 0.5 * time();
+    }
+
+    function rendering(){
+        objectMove(ball);
+
+        requestAnimationFrame(rendering);
+        mesh.rotation.x = ball.rotationX;
+        mesh.rotation.y = ball.rotationY;
+        mesh.rotation.z -= 0.2;
+        mesh.position.x = ball.positionX;
+        mesh.position.y = ball.positionY;
+        mesh.position.z = ball.positionZ;
+
+        renderer.render(scene, camera);
+    }
+
+    
+
+    rendering();
+}
