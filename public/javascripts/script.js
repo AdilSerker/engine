ffec10dc3a0f5e67193f1645b23b65da1e93ba00
Adil <adilserkerov@gmail.com>
2017-12-22 13:21:42 +0300

    refactor

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,74 +1,56 @@
-window.onload = function (){
-    
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    
+window.onload = function (){    
+    const canvas = document.getElementById("canvas");
+    canvas.setAttribute('width', window.innerWidth);
+    canvas.setAttribute('height', window.innerHeight);
+
+    const fpsElem = document.getElementById("fps");
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas, 
+        antialias: true
+    });
+    renderer.setClearColor(0x000000);
+
+    const scene = new THREE.Scene();
+
+    const light = THREE.AmbientLight(0xffffff);
+    scene.add(light);
+
+
+    const camera = new THREE.PerspectiveCamera(
+        60,
+        window.innerWidth / window.innerHeight, 
+        0.1, 
+        10000
+    );
+    camera.position.set(0, 0, 1000);
+
+    const geometry = new THREE.SphereGeometry(50, 15, 15);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff, 
+        vertexColors: THREE.FaceColors
+    });
+
+    const clock = new THREE.Clock();
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
     let ball = {
         rotationX: 0,
         rotationY: 0,
         rotationZ: 0,
-        positionX: -1500,
+        positionX: -3500,
         positionY: 0,
         positionZ: -800
     }
-
-    let canvas = document.getElementById("canvas");
-    canvas.setAttribute('width', width);
-    canvas.setAttribute('height', height);
-
-    let renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
-    renderer.setClearColor(0x000000);
-
-    let scene = new THREE.Scene();
-
-    let light = THREE.AmbientLight(0xffffff);
-    scene.add(light);
-
-
-    let camera = new THREE.PerspectiveCamera(25, width / height, 0.1, 10000);
-    camera.position.set(0, 0, 1000);
-
-    let geometry = new THREE.SphereGeometry(50, 15, 15);
-    let material = new THREE.MeshBasicMaterial({color: 0xffffff, vertexColors: THREE.FaceColors});
-
-    for(let i = 0; i < geometry.faces.length; ++i) {
-        geometry.faces[i].color.setRGB(r(), r(), r());
-    }
-
-    function r(){
-        let rand = Math.random();
-        if(rand > 0.8 && rand < 0.2)
-            return r();
-        return rand;
-    }
-
-    let t0 = 0,
-            deltaTime;
-    function time() {
-        let now = performance.now();
-        deltaTime = now-t0;
-        t0 = now;
-        return deltaTime;
-    }
-
-
-    let mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    function objectMove(obj){
-        obj.positionX += 0.5 * time();
-    }
-    let fpsElem = document.getElementById("fps");
-    console.dir(fpsElem);
-    let then = 0;
-    function rendering(now){
-        now *= 0.001;
-        const deltaTime = now - then;
-        then = now;
+    
+    function rendering(){
+        const deltaTime = clock.getDelta();
         const fps = 1 / deltaTime;
         fpsElem.textContent = fps.toFixed(1);
 
-        objectMove(ball);
+        ball.positionX += 2000 * deltaTime;
 
         requestAnimationFrame(rendering);
         mesh.rotation.x = ball.rotationX;
@@ -80,8 +62,6 @@ window.onload = function (){
 
         renderer.render(scene, camera);
     }
-
-    
 
     rendering();
 }
