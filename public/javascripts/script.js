29c3e2935ee96beec0e8d2c84c58ef2455e422ab
Adil <adilserkerov@gmail.com>
2017-12-23 13:32:31 +0300

    add g

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,9 @@
-window.onload = function (){    
+window.onload = function(){
+    const G = 6.67384e-11;
+    const EARTH_MASS = 5.9722e+24;
+
+    const g = G * EARTH_MASS / Math.pow(6371000, 2);
+     
     const canvas = document.getElementById("canvas");
     canvas.setAttribute('width', window.innerWidth);
     canvas.setAttribute('height', window.innerHeight);
@@ -42,26 +47,42 @@ window.onload = function (){
         rotationZ: 0,
         positionX: -3500,
         positionY: 0,
-        positionZ: -800
-    }
-    
+        positionZ: -800,
+        mass: 2
+    } 
+
+    let counter = 1;
+    let deltaTimeSum = 0;
+
+    let SECONDS = 1;
+    let METER = 1000;
+
+    let v = 0;
+
     function rendering(){
         const deltaTime = clock.getDelta();
         const fps = 1 / deltaTime;
         fpsElem.textContent = fps.toFixed(1);
+        v += g;
+        ball.positionX += v*deltaTime;
+        ball.rotationZ -= 0.2; 
 
-        ball.positionX += 2000 * deltaTime;
-
-        requestAnimationFrame(rendering);
         mesh.rotation.x = ball.rotationX;
         mesh.rotation.y = ball.rotationY;
-        mesh.rotation.z -= 0.2;
+        mesh.rotation.z = ball.rotationZ;
         mesh.position.x = ball.positionX;
         mesh.position.y = ball.positionY;
         mesh.position.z = ball.positionZ;
 
-        renderer.render(scene, camera);
+        renderer.render(scene, camera); 
+        requestAnimationFrame(rendering);
     }
 
     rendering();
 }
+
+function powerNumber(num, pow) {
+    let counter = 0;
+    return pow == 0? 1: num * powerNumber(num, pow-1);
+}
+
