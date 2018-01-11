a52efd6260d09cf12fd36d098eb590b433a318a6
Adil <adilserkerov@gmail.com>
2018-01-11 11:16:37 +0300

    add ObjectConstructor

--- src/script.js
+++ src/script.js
@@ -50,6 +50,67 @@ class ObjectGenerate {
     }
 }
 
+class Spacetime {
+    constructor(){
+        this.canvas_;
+        this.scene_;
+        this.light_;
+        this.render_;
+        this.camera_;
+        this.time_ = new THREE.Clock();
+    }
+    getInstance(){
+        this._setCanvas();
+        this._setRender();
+        this._setScene();
+        this._setCamera();
+
+        return this;
+    }
+
+    render(){
+        this.render_.render(this.scene_, this.camera_);
+    }
+
+    addScene(object){
+        this.scene_.add(object);
+    }
+    deltaTime(){
+        return this.time_.getDelta();
+    }
+    _setCanvas(){
+        this.canvas_ = document.createElement("canvas");
+        document.body.appendChild(this.canvas_);
+        this.canvas_.setAttribute('width', window.innerWidth);
+        this.canvas_.setAttribute('height', window.innerHeight);
+    }
+    _setRender(){
+        this.render_ = new THREE.WebGLRenderer({
+            canvas: this.canvas_, 
+            antialias: true
+        });
+        this.render_.setClearColor(0x000000);
+    }
+    _setScene(){
+        this.scene_ = new THREE.Scene();
+        this.light_ = new THREE.AmbientLight(0xffffff);
+        this.scene_.add(this.light_);
+    }
+    _setCamera(){
+        this.camera_ = new THREE.PerspectiveCamera(
+            60,
+            window.innerWidth / window.innerHeight, 
+            0.1, 
+            15000
+        );
+        this.camera_.position.set(0, -5000, 10000);
+    }
+}
+
+const spacetime = new Spacetime();
+let space = spacetime.getInstance();
+
+
 const SECONDS = 1;
 const METER = 1000;
 const G = 6.67384e-11;
@@ -58,52 +119,16 @@ const g = G * EARTH_MASS / Math.pow(6371000, 2);
 
 let COUNTER = 0;
 
-function getCanvas(){
-    const canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);
-    return canvas;
-}
-const cnvs = getCanvas();
-
-const renderer = new THREE.WebGLRenderer({
-    canvas: cnvs, 
-    antialias: true
-});
-renderer.setClearColor(0x000000);
-
-const scene = new THREE.Scene();
-const light = new THREE.AmbientLight(0xffffff);
-scene.add(light);
-
-const camera = new THREE.PerspectiveCamera(
-    60,
-    window.innerWidth / window.innerHeight, 
-    0.1, 
-    15000
-);
-camera.position.set(0, -5000, 10000);
 
 const ball_2 = new ObjectGenerate(-7000, -2000, 0, );
 ball_2.mass = 5.9722e+24 / (81.3*100000000000);
-scene.add(ball_2.mesh);
+
+space.addScene(ball_2.mesh);
 
 const ball_3 = new ObjectGenerate(-7000, -5000, 0, );
 ball_3.mass = 5.9722e+24 / 10000000000;
-scene.add(ball_3.mesh);
 
-
-const MOON = {
-    position: new THREE.Vector3(0, -1737000000, 0),
-    mass: 5.9722e+24 / 81.3
-}
-
-
-const EARTH = {
-    position: new THREE.Vector3(-6371000000, -6371000000, 0),
-    mass: 5.9722e+24
-}
+space.addScene(ball_3.mesh);
 
 function fpsRender(dt){
     const fpsElem = document.getElementById("fps");
@@ -112,7 +137,7 @@ function fpsRender(dt){
     return fps;
 }
 
-const clock = new THREE.Clock();
+
 
 function gravityForce(m1, m2){
     const mass1 = m1.mass;
@@ -142,13 +167,14 @@ function rotateVec(v, angle) {
 
 let COUNTER_1 = 0;
 function rendering(){
-    const deltaTime = clock.getDelta(); 
-    fpsRender(deltaTime);
+    
+    const dt = space.deltaTime();
+    fpsRender(dt);
 
     let FIRST_SPACE = new THREE.Vector3(1, 0, 0);
     FIRST_SPACE.multiplyScalar(10000000000000000*2);
 
-    ball_2.updatePosition(deltaTime);
+    ball_2.updatePosition(dt);
     
     if (COUNTER_1 < 3 ) {
         ball_2.addVector(FIRST_SPACE);
@@ -156,9 +182,10 @@ function rendering(){
     }
     let gravity_2 = gravityForce(ball_3, ball_2);
 
-    ball_2.updateVector(deltaTime, gravity_2);
+    ball_2.updateVector(dt, gravity_2);
 
-    renderer.render(scene, camera);
+    
+    space.render();
     requestAnimationFrame(rendering);
 }
 
