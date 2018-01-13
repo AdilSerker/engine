9c80a17f001b02e2aaf66d5a418eb8e3072c4fa6
Adil <adilserkerov@gmail.com>
2018-01-13 12:52:41 +0300

    4 moon

--- src/script.js
+++ src/script.js
@@ -11,11 +11,15 @@ const T = 1000;
 
 const orbit = new Vector3(-600, 600, 0);
 const orbit2 = new Vector3(750, -700, 0);
+const orbit3 = new Vector3(-600, 600, 0);
+const orbit4 = new Vector3(-500, 0, 0);
 
 const planet = new ObjectGenerator(0, 0, 0, 100000000000000*T);
 const moon = new ObjectGenerator(400, 400, 0, 1);
 const moon2 = new ObjectGenerator(-300, -300, 0, 1);
-timespace.add(moon2, moon, planet);
+const moon3 = new ObjectGenerator(200, 250, 0, 1);
+const moon4 = new ObjectGenerator(-500, 500, 0, 1);
+timespace.add(moon4, moon3, moon2, moon, planet);
 
 webgl.setRender(timespace.scene);
 
@@ -24,6 +28,8 @@ function renderLoop(){
     if(COUNTER<1){
         moon.updateVector(orbit);
         moon2.updateVector(orbit2);
+        moon3.updateVector(orbit3);
+        moon4.updateVector(orbit4);
         COUNTER++;
     }
 
